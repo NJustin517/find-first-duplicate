f5f42b2f7899a81aba4e338df93171646ae108e4
Justin Luciano <justinlluciano@gmail.com>
2022-01-26 19:55:11 -0800

    completed

--- javascript/find_first_duplicate.js
+++ javascript/find_first_duplicate.js
@@ -1,9 +1,17 @@
 function findFirstDuplicate(arr) {
-  // type your code here
+  let seen = {};
+  for (let i = 0; i < arr.length; i++) {
+    if (seen[arr[i]]) {
+      return arr[i];
+    }
+    seen[arr[i]] = true;
+  }
+  return -1;
 }
 
 if (require.main === module) {
-  // add your own tests in here
+  console.log(findFirstDuplicate([0, 2, 3, 5, 7, 9]));
+  console.log(findFirstDuplicate([1, 2, 3, 4, 1, 3, 2]));
   console.log("Expecting: 3");
   console.log("=>", findFirstDuplicate([2, 1, 3, 3, 2]));
 
